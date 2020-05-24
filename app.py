0f829963bbbe5e228b9f324aff1d5cc24df7b469
Budi Kusumo <thebuddystuff@gmail.com>
2020-05-24 08:45:59 +0700

    Main application file (command file)

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from sqlalchemy_utils import database_exists
+from sqlalchemy.engine.url import make_url
+from db import db, ClientURI, setup_module
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.agama import Agama
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ClientURI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    url = make_url(app.config['SQLALCHEMY_DATABASE_URI'])
+    if not database_exists(url):
+        setup_module()
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Agama, '/agama')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
